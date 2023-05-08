2fb05e3d0fe14bdab34bb47873ac977570f5ae3e
Fabrício <fabricioferreira900@gmail.com>
2023-05-07 21:00:57 -0300

    Ajustes

--- Jogador/jogador.gd
+++ Jogador/jogador.gd
@@ -2,7 +2,28 @@ extends KinematicBody2D
 
 export var movimento = 80
 
+enum { #enumera variáveis
+	MOV,
+	ROL,
+	ATQ
+}
+var estado = MOV
+
+func _ready():
+	$AnimationTree.active = true
+
 func _physics_process(delta):
+	match estado: #É como se fosse um switch basicamente. Faz com que apena um bloco de código rode por vez. Facilita debug
+		MOV:
+			estado_mov(delta)
+		
+		ROL:
+			pass
+		
+		ATQ:
+			estado_atq(delta)
+	
+func estado_mov(delta):
 	var velocidade = Vector2.ZERO
 	
 	#Se tal conjunto de teclas é apertado, x ou y adiciona valor
@@ -23,6 +44,16 @@ func _physics_process(delta):
 	else:
 		$AnimationTree.get("parameters/playback").travel("Andando") #Mover para animação de andar
 		$AnimationTree.set("parameters/Parado/blend_position", velocidade) #Congelar a direção de movimento
-		$AnimationTree.set("parameters/Andando/blend_position", velocidade) #Anima~ção de andar para outros lados
-	
+		$AnimationTree.set("parameters/Andando/blend_position", velocidade) #Animação de andar para outros lados
+		$AnimationTree.set("parameters/Ataque/blend_position", velocidade) #Não mudar direção de ataque. Forçar animação de ataque mesma direção e movimento
 	move_and_slide(movimento * velocidade)
+	
+	if Input.is_action_just_pressed("ataque"):
+		estado = ATQ
+
+func estado_atq(delta):
+	
+	$AnimationTree.get("parameters/playback").travel("Ataque")
+
+func ataque_terminado():
+	estado = MOV
