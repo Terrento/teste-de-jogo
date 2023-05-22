8e20b311f51d976a6c11842b32cff06591133f01
Fabrício <fabricioferreira900@gmail.com>
2023-05-22 00:31:05 -0300

    adaptacoes de nomes e refatoracao

--- Mundo/Grama.gd
+++ Mundo/Grama.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-func criar_grama():
-	var GramaEfeito = load("res://Efeitos/GramaEfeito.tscn") #instanciando cena em uma varíavel
+const GramaEfeito = preload("res://Efeitos/GramaEfeito.tscn")#instanciando cena em uma varíavel
+#Preload e compartilhar // não precisa carregar varias vezes
+func criar_grama_efeito():
 	var gramaEfeito = GramaEfeito.instance()
-	var mundo = get_tree().current_scene
-	mundo.add_child(gramaEfeito)
+	get_parent().add_child(gramaEfeito)
 	gramaEfeito.global_position = global_position
 
 func _on_Hurtbox_area_entered(area):
-	criar_grama()
+	criar_grama_efeito()
 	queue_free()
